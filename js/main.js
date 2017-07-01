97cb866a5551742627c9ca568e5fb39bac94c2ab
Kenneth <kennethsjorgensen@gmail.com>
2017-07-01 20:23:15 +0200

    Roger's portfolio site

---
+++ js/main.js
@@ -0,0 +1,109 @@
+/*
+var imgArray = [
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-441840.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-531657.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-511501.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-133084.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-290365.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-309978.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-191615.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-204149.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-376797.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-297823.jpg',
+    'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-294660.jpg'
+]
+*/
+
+
+var imgArray = [
+    'images/2fugler.jpg',
+    'images/blomst_bw.jpg',
+    'images/fjaera.jpg',
+    'images/flue.jpg',
+    'images/fugl-macro.jpg',
+    'images/fugl.jpg',
+    'images/fugler.jpg',
+    'images/hav.jpg',
+    'images/mane.jpg',
+    'images/ramme.jpg',
+    'images/silhouette.jpg'
+]
+
+document.querySelector('img').src = imgArray[0]
+
+var i = 0
+
+var prev = document.querySelector('.prev-btn')
+var next = document.querySelector('.next-btn')
+var slideCurrent = document.querySelector('.slider-current')
+var slideLength = document.querySelector('.slider-length')
+
+slideCurrent.textContent = i + 1;
+slideLength.textContent = imgArray.length;
+
+next.addEventListener('mousedown', function () {
+
+    if (i === imgArray.length - 1) {
+
+        i = 0;
+        document.querySelector('img').classList.add('hide-img');
+        document.querySelector('img').src = imgArray[i];
+        setTimeout(function () {
+            document.querySelector('img').src = imgArray[i];
+            document.querySelector('img').classList.remove('hide-img');
+        }, 500);
+        slideCurrent.textContent = i + 1;
+
+    } else {
+
+        i++;
+        document.querySelector('img').classList.add('hide-img');
+        setTimeout(function () {
+            document.querySelector('img').src = imgArray[i];
+            document.querySelector('img').classList.remove('hide-img');
+        }, 500);
+        slideCurrent.textContent = i + 1;
+
+    }
+})
+
+prev.addEventListener('mousedown', function () {
+
+    if (i === 0) {
+
+        i = imgArray.length - 1;
+        document.querySelector('img').classList.add('hide-img');
+        setTimeout(function () {
+            document.querySelector('img').src = imgArray[i];
+            document.querySelector('img').classList.remove('hide-img');
+        }, 500);
+
+        slideCurrent.textContent = i + 1;
+        setTimeout(function () {
+            document.querySelector('img').src = imgArray[i];
+            document.querySelector('img').classList.add('text-transition');
+        }, 500);
+
+    } else {
+
+        i--;
+        document.querySelector('img').classList.add('hide-img');
+        setTimeout(function () {
+            document.querySelector('img').src = imgArray[i];
+            document.querySelector('img').classList.remove('hide-img');
+        }, 500);
+        slideCurrent.textContent = i + 1;
+
+    }
+
+})
+
+var content = document.querySelector('.content')
+var nav = document.querySelector('nav')
+var links = document.querySelector('ul')
+
+nav.addEventListener('mousedown', function ()  {
+    content.classList.toggle('skew');
+    document.querySelector('#nav-icon').classList.toggle('open');
+    links.classList.toggle('show-text');
+})
